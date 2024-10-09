6cb10e320f0db10414b803efb1c22df6bf64b32b
Heni1930 <heni.pellei@gmail.com>
2024-10-09 14:10:07 +0200

    little front end commit

---
+++ src/main/java/org/example/MainSceneController.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainSceneController {
+    @FXML
+    void LoginButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SignupButton(ActionEvent event) {
+
+    }
+}
