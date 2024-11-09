6c4c602fe027d05620bc1d6ed11b412357aed27b
Heni1930 <heni.pellei@gmail.com>
2024-11-09 14:12:56 +0100

    login,singup scene basics

--- src/main/java/org/example/SignUpSceneController.java
+++ src/main/java/org/example/SignUpSceneController.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpSceneController {
+
+    @FXML
+    private TextField EmailAdressTextField;
+
+    @FXML
+    private TextField NameTextField;
+
+    @FXML
+    private PasswordField Password;
+
+    @FXML
+    private Button SignUpBt;
+
+    @FXML
+    private TextField SurameTextField;
+
+    @FXML
+    private TextField UsernameTextField;
+
+    @FXML
+    private ComboBox<?> faculty;
+
 }
+
