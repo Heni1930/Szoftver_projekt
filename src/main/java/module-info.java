43a2c4cf3444b15a5dfd80ddacc945fd29bfe185
Fanni117 <vinklerfanni@gmail.com>
2024-11-16 12:57:14 +0100

    Sign Up Data Listing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.compiler;
+    requires java.desktop;
 
     opens org.example to javafx.fxml;
     exports org.example;
