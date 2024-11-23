96588dcd7a096734a0add6687e71da2495d77c74
Fanni117 <vinklerfanni@gmail.com>
2024-11-23 21:45:14 +0100

    LoginButton-Correction

--- src/main/java/org/example/LoginSceneController.java
+++ src/main/java/org/example/LoginSceneController.java
@@ -2,9 +2,16 @@ package org.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginSceneController {
 
@@ -26,6 +33,17 @@ public class LoginSceneController {
     }
 
     public void SignupButton(ActionEvent actionEvent) {
-        //fejlesztés alatt
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLSignUpScene.fxml"));
+            Parent root = loader.load();
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene signUpScene = new Scene(root);
+            currentStage.setScene(signUpScene);
+            currentStage.setTitle("Sign Up");
+            currentStage.centerOnScreen();
+            currentStage.setResizable(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
