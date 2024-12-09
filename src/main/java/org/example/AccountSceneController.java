c11382614bab511a4cf4e26ba6f66be02678f8c0
Fanni117 <vinklerfanni@gmail.com>
2024-12-09 22:30:26 +0100

    Main after Log In

--- src/main/java/org/example/AccountSceneController.java
+++ src/main/java/org/example/AccountSceneController.java
@@ -1,12 +1,17 @@
 package org.example;
 
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 
 public class AccountSceneController {
-    public Circle nameCircle;
 
+    @FXML
     public void SignOut(MouseEvent mouseEvent) {
+        MainSceneController mainController = (MainSceneController) App.getCurrentController();
+        if (mainController != null) {
+            mainController.updateButtonsAfterLogin();
+        }
     }
 
     public void PersonalData(MouseEvent mouseEvent) {
