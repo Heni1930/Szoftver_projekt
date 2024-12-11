1a9607d4e33857ef1f6d69440b855ce157d7bfdd
Fanni117 <vinklerfanni@gmail.com>
2024-12-11 19:22:04 +0100

    ridCorrection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -24,6 +24,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
         primaryStage = stage;
         URL loginFxmlUrl = getClass().getResource("/org/example/FXMLMainScene.fxml");
         FXMLLoader loader = new FXMLLoader(loginFxmlUrl);
